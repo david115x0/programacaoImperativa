dc9489eb8dd1b6a5f3079dfcfcf36333fdcb2f70
david115x0 <davidsilvafs29@gmail.com>
2021-06-13 18:53:17 -0300

    editado

--- microDesafios/callBacks/ex01.js
+++ microDesafios/callBacks/ex01.js
@@ -4,11 +4,9 @@
 function comandos(andar, parar) {
     return andar +  ' e '  + parar;
 }
-let andar = "O carro está andando"
-let parar = "O carro parou"
 
 function acaoCarro(andar, parar, callback){
     return 'comandos : '+ callback(andar, parar)
 }
 
-console.log(acaoCarro(andar, parar, comandos))
+console.log(acaoCarro("O carro está andando", "O carro parou" , comandos))
