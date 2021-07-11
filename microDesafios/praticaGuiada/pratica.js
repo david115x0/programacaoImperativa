67c8e95c84c2566561559782740e0caaa31cdece
david115x0 <davidsilvafs29@gmail.com>
2021-07-11 15:28:52 -0300

    11/07/21

--- microDesafios/praticaGuiada/pratica.js
+++ microDesafios/praticaGuiada/pratica.js
@@ -1,16 +1,27 @@
 // 05/07/2021
 //PRATICA GUIADA
 
-let produtos = [
+const produtos = [
     {nomedoproduto: 'veja caixa com 100u', valordoproduto: 500, qualidadedoproduto: 90, status: true},
     {nomedoproduto: 'Leite Uht Lider 1l Integral caixa com 400u', valordoproduto: 100, qualidadedoproduto: 50, status: false},
     {nomedoproduto: 'Arroz Raroz 5kg fardo com 25u', valordoproduto: 500, qualidadedoproduto: 65, status: true},
-]
-const carrinho = produtos.filter((produto) =>{
-    let valor = produto.valordoproduto
-    let qualidade = produto.qualidadedoproduto
-    let status = produto.status
-})
+];
 
+produtos.valordoproduto;
 
-console.log(selecionar(produtos[0]));
+let carrinho = produtos.filter( selecionar => {
+    if(selecionar.valordoproduto>=482 && selecionar.valordoproduto<=1600) {
+        return selecionar.valordoproduto;
+    }else if(selecionar.qualidadedoproduto<60){
+     return selecionar.qualidadedoproduto=false;
+    }else if (selecionar.status != true){return selecionar.status=false;}
+    
+});
+
+console.log(carrinho);
+
+let calcular = carrinho.reduce((resultado, quantidade)=>{
+    return (resultado+quantidade.valordoproduto);
+},0)
+console.log(calcular);
+
